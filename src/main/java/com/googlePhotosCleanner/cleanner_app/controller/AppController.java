a96e77214566bcb5d14fa9f08fa133883e717c23
alejandro rojas <alerojasdev@gmail.com>
2024-08-03 20:17:16 -0400

    change class name and add a new endpoint
    Delete some test files

--- src/main/java/com/googlePhotosCleanner/cleanner_app/controller/AppController.java
+++ src/main/java/com/googlePhotosCleanner/cleanner_app/controller/AppController.java
@@ -1,22 +1,29 @@
 package com.googlePhotosCleanner.cleanner_app.controller;
 
+import com.googlePhotosCleanner.cleanner_app.service.PhotosAPI;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import java.io.IOException;
+import java.util.List;
+
 @Controller
 public class AppController {
 
-    @RequestMapping("photosCleaner/landingPage")
-    public String landingPage(){
-        return "landingPage";
-    }
-
-    @RequestMapping("photosCleaner/privacyPolicy") //for login page
-    public String privacyPolicy(){
-        return "privacyPolicy";
-    }
-
-    @RequestMapping("photosCleaner/termsOfService") //for login page
-    public String termsOfService(){
-        return "termsOfService";
+    @Autowired
+    PhotosAPI photosAPI;
+    @GetMapping("/photosOrganizer")
+    public String getPhotos(Model model, @AuthenticationPrincipal OidcUser principal) throws IOException {
+        List<String> imageUrls = photosAPI.getAllMediaItemsFromUser(principal.getName());
+        System.out.println(" ");
+        System.out.println("-----------------------------------------------------------");
+        System.out.println(imageUrls);
+        System.out.println("-----------------------------------------------------------");
+        System.out.println(" ");
+        model.addAttribute("imageUrls", imageUrls);
+        return "visualizer";
     }
 }
